a49b5f3a7b19d8682b0d27fe0434c7dee297d1a4
raphael-dario <raphael@mydario.com>
2022-05-24 16:22:54 +0300

    implement secrets gradle plugin

--- app/src/main/java/com/r/cohen/askwiki/repos/witai/WitAiService.kt
+++ app/src/main/java/com/r/cohen/askwiki/repos/witai/WitAiService.kt
@@ -1,5 +1,6 @@
 package com.r.cohen.askwiki.repos.witai
 
+import com.r.cohen.askwiki.BuildConfig
 import com.r.cohen.askwiki.repos.witai.models.WitAiResponse
 import org.json.JSONObject
 import retrofit2.http.GET
@@ -8,14 +9,13 @@ import retrofit2.http.Query
 
 private const val apiServiceVersion = "20220410"
 private const val defaultContext = "{\"locale\"=\"en_US\",\"timezone\":\"America/Los_Angeles\"}"
-private const val apiKey = "GYQIRSGERMILQYCBOKODG5MJBRD4LLBM"
 
 interface WitAiService {
     companion object {
         const val witaiApiUrl = "https://api.wit.ai/"
     }
 
-    @Headers("Authorization: Bearer $apiKey")
+    @Headers("Authorization: Bearer ${BuildConfig.witAiApiKey}")
     @GET("message")
     suspend fun queryMessage(
         @Query("q") msg: String,
